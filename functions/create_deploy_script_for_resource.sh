05adecbadd423f1be24ef5dcc4eb017240f14fae
Teri Radichel <github@pwn.fyi>
2025-06-29 17:30:15 -0400

    Update create_deploy_script_for_resource.sh

--- functions/create_deploy_script_for_resource.sh
+++ functions/create_deploy_script_for_resource.sh
@@ -86,4 +86,22 @@ generate_resource_code() {
     echo "deploy_cloudformation_stack \$STACK_NAME \$TEMPLATE_FILE_PATH \$ENCODED_PARAMETERS \$IAM_CAPABILITY" >> "$SCRIPT_FILE_PATH"
 }
 
+create_deploy_script_for_resource() {
+    local SERVICE_NAME="$1"
+    local RESOURCE_NAME="$2"
+    
+    local SCRIPT_FILE_PATH=$(get_script_file_path $SERVICE_NAME $RESOURCE_NAME)
+    local TEMPLATE_FILE_PATH=$(get_template_file_path $SERVICE_NAME $RESOURCE_NAME)
+    
+    # Create the script file with shebang
+    echo '#!/bin/bash -e' > "$SCRIPT_FILE_PATH"
+    
+    # Make the script executable
+    chmod +x "$SCRIPT_FILE_PATH"
+    
+    # Generate the resource code
+    generate_resource_code "$SERVICE_NAME" "$RESOURCE_NAME" "$SCRIPT_FILE_PATH" "$TEMPLATE_FILE_PATH"
+    
+    echo "Created deployment script at $SCRIPT_FILE_PATH"
+}
 
