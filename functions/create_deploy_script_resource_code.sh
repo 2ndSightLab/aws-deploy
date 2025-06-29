dfadf98804306d813fee29d186562bd55fdffc39
Teri Radichel <github@pwn.fyi>
2025-06-29 18:47:50 -0400

    Update create_deploy_script_resource_code.sh

--- functions/create_deploy_script_resource_code.sh
+++ functions/create_deploy_script_resource_code.sh
@@ -60,6 +60,9 @@ create_deploy_script_resource_code() {
     if [ ! -s "$SCRIPT_FILE_PATH" ]; then
         echo "Error: The generated script file is empty." >&2
         exit 1
+    else
+       echo "Property code written:"
+       cat $SCRIPT_FILE_PATH
     fi
 
     # Add base64 encoding of parameter overrides with proper handling of special characters
