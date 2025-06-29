a6e7146d092db04dc814cde2706018d99b524b0f
Teri Radichel <github@pwn.fyi>
2025-06-29 16:23:15 -0400

    Update list_service_names.sh

--- functions/list_service_names.sh
+++ functions/list_service_names.sh
@@ -1,4 +1,11 @@
-#!/bin/bash
-list_service_names(){
-  aws cloudformation list-types --visibility PUBLIC --type RESOURCE --output json | jq -r '.TypeSummaries[].TypeName' | grep -o 'AWS::[^:]*' | sed 's/AWS:://' | sort -u
+#!/bin/bash -e
+list_service_resource_names() {
+  SERVICE_NAME="$1"
+
+  aws cloudformation list-types \
+    --visibility PUBLIC \
+    --type RESOURCE \
+    --filters TypeNamePrefix=AWS::${SERVICE_NAME}:: \
+    --query 'TypeSummaries[].TypeName' \
+    --output text | sed "s/AWS::${SERVICE_NAME}:://g"
 }
