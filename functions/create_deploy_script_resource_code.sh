6ad302e51c0ea881f44344baa662a67817383ddb
Teri Radichel <github@pwn.fyi>
2025-06-30 04:26:04 -0400

    Update create_deploy_script_resource_code.sh

--- functions/create_deploy_script_resource_code.sh
+++ functions/create_deploy_script_resource_code.sh
@@ -19,7 +19,7 @@ create_deploy_script_resource_code() {
      echo "echo \"Enter property values for $RESOURCE_TYPE:\""  >> "$SCRIPT_FILE_PATH"
      while read -r property; do
 
-            echo "Processing property: $property in script resource code"
+            #echo "Processing property: $property in script resource code"
         
             # Check if property is in the read-only list
             if echo "$readOnlyProps" | grep -q "^$property$"; then
