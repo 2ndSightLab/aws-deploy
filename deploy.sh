29e6205fd82c3fbd0a7e4ca1a1c5ce1d38b098f1
Teri Radichel <github@pwn.fyi>
2025-06-30 16:45:43 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,8 @@ echo "source all the files in the functions directory"
 for file in functions/*; do [ -f "$file" ] && source "$file"; done
 
 REGION=$(get_region)
-echo "CURRENT REGION: $REGION."
-
 IDENTITY_ARN=$(get_current_identity_arn)
-echo "IDENITTY_ARN: $IDENTITY_ARN"
-
 IDENTITY_NAME=$(get_identity_name_from_arn $IDENTITY_ARN)
-echo "IDENTITY_NAME: $IDENTITY_NAME"
 
 echo "Enter environment name (prod, dev, test):"
 read ENV_NAME 
@@ -51,13 +46,6 @@ echo "Initializing...please wait..."
 STACK_NAME=$(get_stack_name "$ENV_NAME" "$IDENTITY_NAME" "$SERVICE_NAME" "$RESOURCE_NAME" "$NAME")
 STACK_RESOURCE_NAME=$(get_stack_resource_name "$ENV_NAME" "$SERVICE_NAME" "$RESOURCE_NAME" "$NAME")
 
-echo "ENV: $ENV_NAME"
-echo "IDENTITY_ARN: $IDENTITY_ARN"
-echo "IDENTITY_NAME: $IDENTITY_NAME"
-echo "REGION: $REGION"
-echo "STACK_NAME: $STACK_NAME"
-echo "STACK_RESOURCE_NAME: $STACK_RESOURCE_NAME"
-
 TEMPLATE_FILE_PATH=$(get_template_file_path $SERVICE_NAME $RESOURCE_NAME)
 create_cloudformation_template $SERVICE_NAME $RESOURCE_NAME
 if [ ! -f $TEMPLATE_FILE_PATH ]; then echo "$TEMPLATE_FILE_PATH does not exist. Exiting."; exit; fi
@@ -65,6 +53,17 @@ if [ ! -f $TEMPLATE_FILE_PATH ]; then echo "$TEMPLATE_FILE_PATH does not exist. 
 SCRIPT_FILE_PATH=$(get_script_file_path $SERVICE_NAME $RESOURCE_NAME)
 create_deploy_script_for_resource $SERVICE_NAME $RESOURCE_NAME
 if [ ! -f $SCRIPT_FILE_PATH ]; then echo "$SCRIPT_FILE_PATH does not exist. Exiting."; exit; fi
+
+echo ""
+echo "ENV: $ENV_NAME"
+echo "IDENTITY_ARN: $IDENTITY_ARN"
+echo "IDENTITY_NAME: $IDENTITY_NAME"
+echo "REGION: $REGION"
+echo "STACK_NAME: $STACK_NAME"
+echo "STACK_RESOURCE_NAME: $STACK_RESOURCE_NAME"
+echo "TEMPLATE FILE: $TEMPLATE_FILE_PATH"
+echo "SCRIPT: $SCRIPT_FILE_PATH"
+echo ""
 
 echo "Do you want to deploy the resource now (y)?"
 read ok
