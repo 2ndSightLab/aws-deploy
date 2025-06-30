ecd7d8e5be31deae858037a2e95aaf81680f3c6c
Teri Radichel <github@pwn.fyi>
2025-06-29 22:33:46 -0400

    Update get_resource_properties_schema.sh

--- functions/get_resource_properties_schema.sh
+++ functions/get_resource_properties_schema.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 #resource_type: "AWS::$SERVICE_NAME::$RESOURCE_NAME"
-get_resource_property_schema(){
+get_resource_schema(){
   resource_type="$1"
   
   schema=$(aws cloudformation describe-type --type RESOURCE --type-name "$resource_type" --query 'Schema' --output json)
