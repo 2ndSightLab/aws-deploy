895ca42ee56243ffe9735b7bb2e123d018b2d8cb
Teri Radichel <github@pwn.fyi>
2025-06-30 03:08:20 -0400

    Delete functions/get_resource_properties_json.sh

--- functions/get_resource_properties_json.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-get_resource_properties_json(){
-  SCHEMA="$1"
-  echo "hello"
-  properties_json=$(jq -r 'if type == "string" then fromjson else . end | .properties' <<< "$SCHEMA") 
-  echo $properties_json
-  
-}
