763c4ca1857813c5ea14ebef73b2580b844b99a6
Teri Radichel <github@pwn.fyi>
2025-06-30 02:29:36 -0400

    Update get_resource_properties_json.sh

--- functions/get_resource_properties_json.sh
+++ functions/get_resource_properties_json.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
-get_resource_properties_from_json(){
+get_resource_properties_json(){
   SCHEMA="$1"
-  echo $(jq -r 'fromjson | .properties' <<< "$SCHEMA")
+  local properties_json=$(jq -r 'if type == "string" then fromjson else . end | .properties' <<< "$SCHEMA")
+  echo $properties_json
 }
