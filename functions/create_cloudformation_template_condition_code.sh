2bc821c65a9f31206aaa0069129c02864bc65e48
Teri Radichel <github@pwn.fyi>
2025-06-30 02:43:18 -0400

    Update create_cloudformation_template_condition_code.sh

--- functions/create_cloudformation_template_condition_code.sh
+++ functions/create_cloudformation_template_condition_code.sh
@@ -14,26 +14,39 @@ create_cloudformation_template_condition_code(){
     local readOnlyProps=$(jq -r 'if type == "string" then fromjson else . end | if has("readOnlyProperties") then .readOnlyProperties[] else empty end' <<< "$SCHEMA" | sed 's|/properties/||g')
 
     for prop_info in $properties_info; do
-        IFS=':' read -r prop_name prop_type is_required min_length <<< "$prop_info"
-        # Only create conditions for optional properties
-        if [ "$is_required" = "false" ] && [ "$min_length" -lt 1 ]; then
-            echo "  ${prop_name}Condition:" >> "$TEMPLATE_FILE_PATH"
-            if [ "$prop_type" = "array" ]; then
-                # For array types, check if the array is empty using Fn::Join
-                echo "    Fn::Not:" >> "$TEMPLATE_FILE_PATH"
-                echo "      - Fn::Equals:" >> "$TEMPLATE_FILE_PATH"
-                echo "          - Fn::Join:" >> "$TEMPLATE_FILE_PATH"
-                echo "              - ''" >> "$TEMPLATE_FILE_PATH"
-                echo "              - Ref: $prop_name" >> "$TEMPLATE_FILE_PATH"
-                echo "          - ''" >> "$TEMPLATE_FILE_PATH"
-            else
-                # For non-array types, check if the value is not empty
-                echo "    Fn::Not:" >> "$TEMPLATE_FILE_PATH"
-                echo "      - Fn::Equals:" >> "$TEMPLATE_FILE_PATH"
-                echo "          - Ref: $prop_name" >> "$TEMPLATE_FILE_PATH"
-                echo "          - ''" >> "$TEMPLATE_FILE_PATH"
+
+        local ref=$(echo "$properties_json" | jq -r --arg prop "$property" '.[$prop]["$ref"]')
+ 
+        if [[ -n "$ref" && "$ref" != "null" ]]; then
+            echo "Processing complex type: $ref"
+            local object_schema=$(jq -r --arg defname "$property" 'fromjson | .definitions[$defname]' <<< "$SCHEMA") 
+            local object_schema_b64=$(echo "$object_schema" | base64)
+            create_cloudformation_template_condition_code "$property" "$object_schema_b64" "$TEMPLATE_FILE_PATH"
+        else
+            local type=$(echo "$properties_json" | jq -r --arg prop "$property" '.[$prop].type')
+            local required=$(echo "$properties_json" | jq -r --arg prop "$property" '.[$prop]? // {} | .required? | index($prop) | (. >= 0) | tostring')
+
+            IFS=':' read -r prop_name prop_type is_required min_length <<< "$prop_info"
+            # Only create conditions for optional properties
+            if [[ "$required" == "true" ]]; then
+                echo "  ${prop_name}Condition:" >> "$TEMPLATE_FILE_PATH"
+                if [ "$type" == "array" ]; then
+                    # For array types, check if the array is empty using Fn::Join
+                    echo "    Fn::Not:" >> "$TEMPLATE_FILE_PATH"
+                    echo "      - Fn::Equals:" >> "$TEMPLATE_FILE_PATH"
+                    echo "          - Fn::Join:" >> "$TEMPLATE_FILE_PATH"
+                    echo "              - ''" >> "$TEMPLATE_FILE_PATH"
+                    echo "              - Ref: $prop_name" >> "$TEMPLATE_FILE_PATH"
+                    echo "          - ''" >> "$TEMPLATE_FILE_PATH"
+                else
+                    # For non-array types, check if the value is not empty
+                    echo "    Fn::Not:" >> "$TEMPLATE_FILE_PATH"
+                    echo "      - Fn::Equals:" >> "$TEMPLATE_FILE_PATH"
+                    echo "          - Ref: $prop_name" >> "$TEMPLATE_FILE_PATH"
+                    echo "          - ''" >> "$TEMPLATE_FILE_PATH"
+                fi
             fi
-        fi
+       fi
     done
     echo "" >> "$TEMPLATE_FILE_PATH"
 
