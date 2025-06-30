2a7d1af92449c464f2e0efb92754805c06a23c41
Teri Radichel <github@pwn.fyi>
2025-06-30 03:17:10 -0400

    Update create_cloudformation_template_resource_code.sh

--- functions/create_cloudformation_template_resource_code.sh
+++ functions/create_cloudformation_template_resource_code.sh
@@ -4,7 +4,8 @@ create_cloudformation_template_resource_code(){
     local RESOURCE_TYPE="$1"
     local SCHEMA_B64="$2"
     local TEMPLATE_FILE_PATH="$3"
-
+    local indent=""
+    
     if [[ -z \"\$RESOURCE_TYPE\" ]]; then
        echo "Error: Resource type is not set."
        exit
@@ -19,29 +20,33 @@ create_cloudformation_template_resource_code(){
          echo "Skipping read-only property: $property" 
          continue
       fi
+      
+      local required=$(echo "$properties_json" | jq -r --arg prop "$property" '.[$prop]? // {} | .required? | index($prop) | (. >= 0) | tostring')
+
+      #always print property name
+      echo "$indent      $property:" >> "$TEMPLATE_FILE_PATH"
 
       local ref=$(echo "$properties_json" | jq -r --arg prop "$property" '.[$prop]["$ref"]')
  
         if [[ -n "$ref" && "$ref" != "null" ]]; then
+            
+            indent="$indent      "
             echo "Processing complex type: $ref"
             local object_schema=$(jq -r --arg defname "$property" 'fromjson | .definitions[$defname]' <<< "$SCHEMA") 
             local object_schema_b64=$(echo "$object_schema" | base64)
-            create_cloudformation_template_resource_code "$property" "$object_schema_b64" "$TEMPLATE_FILE_PATH"
+            create_cloudformation_template_resource_code "$property" "$object_schema_b64" "$TEMPLATE_FILE_PATH" "$indent"
+            
         else
             local type=$(echo "$properties_json" | jq -r --arg prop "$property" '.[$prop].type')
-            local required=$(echo "$properties_json" | jq -r --arg prop "$property" '.[$prop]? // {} | .required? | index($prop) | (. >= 0) | tostring')
             if [[ "$required" == "true" ]]; then   
-    
                 # Required properties are always included
-                echo "      $property:" >> "$TEMPLATE_FILE_PATH"
-                echo "        Ref: $property" >> "$TEMPLATE_FILE_PATH"
+                echo "$indent        Ref: $property" >> "$TEMPLATE_FILE_PATH"
             else
                 # Optional properties are conditionally included
-                echo "      $property:" >> "$TEMPLATE_FILE_PATH"
-                echo "        Fn::If:" >> "$TEMPLATE_FILE_PATH"
-                echo "          - ${property}Condition" >> "$TEMPLATE_FILE_PATH"
-                echo "          - Ref: $property" >> "$TEMPLATE_FILE_PATH"
-                echo "          - Ref: AWS::NoValue" >> "$TEMPLATE_FILE_PATH"
+                echo "$indent        Fn::If:" >> "$TEMPLATE_FILE_PATH"
+                echo "$indent          - ${property}Condition" >> "$TEMPLATE_FILE_PATH"
+                echo "$indent          - Ref: $property" >> "$TEMPLATE_FILE_PATH"
+                echo "$indent          - Ref: AWS::NoValue" >> "$TEMPLATE_FILE_PATH"
             fi
         fi
     done < <(echo "$properties_json" | jq -r 'keys[]')
