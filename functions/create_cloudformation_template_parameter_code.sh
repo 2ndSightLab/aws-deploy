94de6c1c334ea719cfd5e09d8347db1635bf92f0
Teri Radichel <github@pwn.fyi>
2025-06-30 02:57:55 -0400

    Update create_cloudformation_template_parameter_code.sh

--- functions/create_cloudformation_template_parameter_code.sh
+++ functions/create_cloudformation_template_parameter_code.sh
@@ -8,10 +8,17 @@ create_cloudformation_template_parameter_code(){
        echo "Error: Resource type is not set."
        exit
     fi
-    
+
     local SCHEMA=$(echo "$SCHEMA_B64" | base64 -d)
     local properties_json=$(get_resource_properties_json $SCHEMA)
     local readOnlyProps=$(get_read_only_properties $SCHEMA)
+
+    echo "Parameters Schema:"
+    echo $SCHEMA
+    echo "Parameters properties:"
+    echo $properties_json
+    echo "Parameter read only props"
+    echo "$readOnlyProps
     
     #local properties_info=$(jq -r 'if type == "string" then fromjson else . end | .properties' <<< "$SCHEMA")
     #local readOnlyProps=$(jq -r 'if type == "string" then fromjson else . end | if has("readOnlyProperties") then .readOnlyProperties[] else empty end' <<< "$SCHEMA" | sed 's|/properties/||g')
