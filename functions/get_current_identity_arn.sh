226109ed01c65744a9cbee7aeea248f609caa8e4
Teri Radichel <github@pwn.fyi>
2025-07-01 16:37:16 -0400

    Update get_current_identity_arn.sh

--- functions/get_current_identity_arn.sh
+++ functions/get_current_identity_arn.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
 get_current_identity_arn() {
     local ENV_PROFILE="$1"
-
+    local REGION="$2"
+    
     if [ -z "$ENV_PROFILE" ]; then echo "$ENV_PROFILE not set in get_current_identity_arn"; fi
+    if [ -z "$REGION" ]; then echo "$REGION not set in get_current_identity_arn"; fi
     
     local arn
-    arn=$(aws sts get-caller-identity --query 'Arn' --profile $ENV_PROFILE --output text)
+    arn=$(aws sts get-caller-identity --query 'Arn' --profile $ENV_PROFILE --region $REGION --output text)
     if [ $? -ne 0 ]; then
         echo "Error: Failed to retrieve identity ARN. Check your AWS credentials and permissions." >&2
         exit 1
