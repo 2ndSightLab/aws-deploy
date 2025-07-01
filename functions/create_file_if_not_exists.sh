fa1b69f1e29f0852126a0d27b6ee771524dbfae1
Teri Radichel <github@pwn.fyi>
2025-07-01 17:08:14 -0400

    Update create_file_if_not_exists.sh

--- functions/create_file_if_not_exists.sh
+++ functions/create_file_if_not_exists.sh
@@ -2,6 +2,8 @@
 create_file_if_not_exists() {
     local file_path="$1"
     
+    if [ -z "$file_path" ]; then echo "$file_path not set in create_file_if_not_exists" >&2; exit 1; fi
+        
     # Check if the file exists
     if [ ! -e "$file_path" ]; then
        mkdir -p "$(dirname "$file_path")"
