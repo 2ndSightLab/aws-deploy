9ce2f15d873471b6804dab329140983b392f40d5
Teri Radichel <github@pwn.fyi>
2025-07-01 16:23:00 -0400

    Update get_resource_schema.sh

--- functions/get_resource_schema.sh
+++ functions/get_resource_schema.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 #resource_type: "AWS::$SERVICE_NAME::$RESOURCE_NAME"
 get_resource_schema(){
-  resource_type="$1"
+  local resource_type="$1"
+  local ENV_PROFILE="$2"
   
-  schema=$(aws cloudformation describe-type --type RESOURCE --type-name "$resource_type" --query 'Schema' --output json)
+  schema=$(aws cloudformation describe-type --type RESOURCE --type-name "$resource_type" --query 'Schema' --profile $ENV_PROFILE --output json)
   echo $schema
 }
