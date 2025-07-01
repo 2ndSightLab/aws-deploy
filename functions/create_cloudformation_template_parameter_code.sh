b065c842f8b9c2d46fb469091723295c971988aa
Teri Radichel <github@pwn.fyi>
2025-07-01 17:04:47 -0400

    Update create_cloudformation_template_parameter_code.sh

--- functions/create_cloudformation_template_parameter_code.sh
+++ functions/create_cloudformation_template_parameter_code.sh
@@ -4,10 +4,9 @@ create_cloudformation_template_parameter_code(){
     local SCHEMA_B64="$2"
     local TEMPLATE_FILE_PATH="$3"
     
-    if [[ -z \"\$RESOURCE_TYPE\" ]]; then
-       echo "Error: Resource type is not set."
-       exit
-    fi
+    if [ -z $RESOURCE_TYPE ]; then echo "$RESOURCE_TYPE not set in create_cloudformation_template_parameter_code" >&2; exit 1; fi
+    if [ -z $SCHEMA_B64 ]; then echo "$SCHEMA_B64 not set in create_cloudformation_template_parameter_code" >&2; exit 1; fi
+    if [ -z $TEMPLATE_FILE_PATH ]; then echo "$TEMPLATE_FILE_PATH not set in create_cloudformation_template_parameter_code" >&2; exit 1; fi
 
     local SCHEMA=$(echo "$SCHEMA_B64" | base64 -d)
     local properties_json=$(jq -r 'if type == "string" then fromjson else . end | .properties' <<< "$SCHEMA") 
