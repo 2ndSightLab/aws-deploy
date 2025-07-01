86349c53feb74affc51412990ed1a11aa0ac171a
Teri Radichel <github@pwn.fyi>
2025-07-01 17:17:55 -0400

    Update list_service_resource_names.sh

--- functions/list_service_resource_names.sh
+++ functions/list_service_resource_names.sh
@@ -1,5 +1,11 @@
 list_service_resource_names() {
   SERVICE_NAME="$1"
+  ENV_PROFILE="$2"
+  REGION="$3"
+  
+  if [ -z "$SERVICE_NAME" ]; then echo "$SERVICE_NAME not set in list_service_resource_names" >&2; exit 1; fi
+  if [ -z "$ENV_PROFILE" ]; then echo "$ENV_PROFILE not set in list_service_resource_names" >&2; exit 1; fi 
+  if [ -z "$REGION" ]; then echo "$REGION not set in list_service_resource_names" >&2; exit 1; fi
 
   aws cloudformation list-types \
     --visibility PUBLIC \
