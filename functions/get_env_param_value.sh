faf0767bc04c148c5f5ef351a6a0666a802a8f1f
Teri Radichel <github@pwn.fyi>
2025-07-01 17:09:55 -0400

    Update get_env_param_value.sh

--- functions/get_env_param_value.sh
+++ functions/get_env_param_value.sh
@@ -3,6 +3,9 @@ get_env_param_value(){
   env_file_path="$1"
   param_name="$2"
 
+  if [ -z "$env_file_path" ]; then echo "$env_file_path not set in get_env_param_value" >&2; exit 1; fi
+  if [ -z "$param_name" ]; then echo "$param_name not set in get_env_param_value" >&2; exit 1; fi
+
   if [ "$env_file_path" == "" ] || [ "$param_name" == "" ]; then
      echo "Error: env_file_path and param_name must be set." >&2
      exit 1
