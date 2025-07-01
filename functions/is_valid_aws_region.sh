5fbbd81278d4c267a3f0ebd415323712191ea9c9
Teri Radichel <github@pwn.fyi>
2025-07-01 16:59:53 -0400

    Update is_valid_aws_region.sh

--- functions/is_valid_aws_region.sh
+++ functions/is_valid_aws_region.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 is_valid_aws_region() {
-    local region_name=$1
+    local REGION=$1
     local ENV_PROFILE=$2
     
-    if [ -z "$ENV_PROFILE" ]; then echo "ENV_PROFILE not set in is_valid_aws_region"; fi
-    if [ -z "$region_name" ]; then echo "region_name not set in is_valid_aws_region"; fi
-    
-    # Check if region name is provided
-    if [ -z "$region_name" ]; then
-        echo "Error: Region name must be provided." >&2
-        return 1
-    fi
+    if [ -z "$ENV_PROFILE" ]; then echo "$ENV_PROFILE not set in deploy_cloudformation_stack" >&2; exit 1; fi
+    if [ -z "$REGION" ]; then echo "$REGION not set in deploy_cloudformation_stack" >&2; exit 1; fi
     
     # Fetch the list of valid AWS regions using us-east-1 so we know we're starting with a valid region
     # Presuming here the user has access to us-east-1. If you change this it should be hard-coded to a valid region.
@@ -23,10 +17,10 @@ is_valid_aws_region() {
     fi
     
     # Check if the region name is in the list
-    if [[ $aws_regions =~ (^|[[:space:]])$region_name($|[[:space:]]) ]]; then
+    if [[ $aws_regions =~ (^|[[:space:]])$REGION($|[[:space:]]) ]]; then
         return 0  # Valid region
     else
-        echo "Error: '$region_name' is not a valid AWS region." >&2
+        echo "Error: '$REGION' is not a valid AWS region." >&2
         return 1  # Invalid region
     fi
 }
