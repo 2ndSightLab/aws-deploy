4af8f4a6c187029899e2b9796ffbf5c761bca58d
Teri Radichel <github@pwn.fyi>
2025-07-01 17:15:19 -0400

    Update get_template_file_name.sh

--- functions/get_template_file_name.sh
+++ functions/get_template_file_name.sh
@@ -3,6 +3,9 @@ get_template_file_path(){
     local SERVICE_NAME="$1"
     local RESOURCE_NAME="$2" 
 
+    if [ -z "$SERVICE_NAME" ]; then echo "$SERVICE_NAME not set in get_template_file_path" >&2; exit 1; fi
+    if [ -z "$RESOURCE_NAME" ]; then echo "$RESOURCE_NAME not set in get_template_file_path" >&2; exit 1; fi
+
     local DIR_PATH="resources/$SERVICE_NAME"
     
     # Create the directory structure if it doesn't exist
