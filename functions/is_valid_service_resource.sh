5345fbed99e5a0cd3e75c288414077ce762b2f25
Teri Radichel <github@pwn.fyi>
2025-07-01 17:01:46 -0400

    Update is_valid_service_resource.sh

--- functions/is_valid_service_resource.sh
+++ functions/is_valid_service_resource.sh
@@ -4,15 +4,17 @@ is_valid_service_resource() {
   SERVICE_NAME="$1"
   RESOURCE_NAME="$2"
   ENV_PROFILE=$3
+  REGION=$4
 
-  if [ -z "$SERVICE_NAME" ]; then echo "$REGION not set in is_valid_service_resource" >&2; exit 1; fi
+  if [ -z "$SERVICE_NAME" ]; then echo "$SERVICE_NAME not set in is_valid_service_resource" >&2; exit 1; fi
+  if [ -z "$RESOURCE_NAME" ]; then echo "$RESOURCE_NAME not set in is_valid_service_resource" >&2; exit 1; fi
   if [ -z "$ENV_PROFILE" ]; then echo "$ENV_PROFILE not set in is_valid_service_resource" >&2; exit 1; fi
   if [ -z "$REGION" ]; then echo "$REGION not set in is_valid_service_resource" >&2; exit 1; fi
 
   RESOURCE_EXISTS=$(aws cloudformation list-types --visibility PUBLIC --type RESOURCE \
   --filters TypeNamePrefix="AWS::${SERVICE_NAME}::${RESOURCE_NAME}" \
   --query "TypeSummaries[?TypeName=='AWS::${SERVICE_NAME}::${RESOURCE_NAME}'].TypeName" \
-  --profile $ENV_PROFILE \
+  --profile $ENV_PROFILE --region $REGION \
   --output text)
   
   if [ -z "$RESOURCE_EXISTS" ]; then
