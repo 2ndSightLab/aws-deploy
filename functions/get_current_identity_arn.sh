1f746ac6ed65dd8d650e992f605eb71476e10853
Teri Radichel <github@pwn.fyi>
2025-07-01 16:25:19 -0400

    Update get_current_identity_arn.sh

--- functions/get_current_identity_arn.sh
+++ functions/get_current_identity_arn.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 get_current_identity_arn() {
+    local ENV_PROFILE="$1"
+
+    if [ -z "$ENV_PROFILE" ]; then echo "$ENV_PROFILE not set in get_current_identity_arn"; fi
+    
     local arn
     arn=$(aws sts get-caller-identity --query 'Arn' --profile $ENV_PROFILE --output text)
     if [ $? -ne 0 ]; then
