39345bfaa23b3b71d95a5993b4c7fba8dd37a481
Teri Radichel <github@pwn.fyi>
2025-07-01 17:19:06 -0400

    Update set_env_param_value.sh

--- functions/set_env_param_value.sh
+++ functions/set_env_param_value.sh
@@ -2,12 +2,11 @@
 set_env_param_value(){
   env_file_path="$1"
   param_name="$2"
-  value"$3"
+  value="$3"
 
-  if [ -z "$env_file_path" ] || [ -z "$param_name" ] || [ -z "$value" ]; then
-     echo "Error: env_file_path, param_name, and value must all be provided." >&2
-     exit 1
-  fi
+  if [ -z "$env_file_path" ]; then echo "$env_file_path not set in set_env_param_value" >&2; exit 1; fi 
+  if [ -z "$param_name" ]; then echo "$param_name not set in set_env_param_value" >&2; exit 1; fi
+  if [ -z "$value" ]; then echo "$value not set in set_env_param_value" >&2; exit 1; fi 
 
   #set the parameter value
   sed -i "s/^${param_name}=.*/${param_name}=\"${value}\"/" "$env_file_path"
