7ae2425d6cbfc19525ff3a6a51ee5bf70b56322c
Teri Radichel <github@pwn.fyi>
2025-07-01 16:51:56 -0400

    Update create_deploy_script_for_resource.sh

--- functions/create_deploy_script_for_resource.sh
+++ functions/create_deploy_script_for_resource.sh
@@ -17,7 +17,7 @@ create_deploy_script_for_resource() {
     local RESOURCE_TYPE="AWS::$SERVICE_NAME::$RESOURCE_NAME"
 
     # Get properties, types, descriptions, enum values, and minimum lengths for the resource type
-    local SCHEMA=$(get_resource_schema $RESOURCE_TYPE $ENV_PROFILE)
+    local SCHEMA=$(get_resource_schema $RESOURCE_TYPE $ENV_PROFILE $REGION)
 
     local SCHEMA_B64=$(echo "$SCHEMA" | base64)
     
@@ -60,9 +60,9 @@ create_deploy_script_for_resource() {
     # Deploy CloudFormation stack
     echo "# Deploy CloudFormation stack" >> "$SCRIPT_FILE_PATH"
     echo "if [[ -z \"\$PARAMETER_OVERRIDES\" ]]; then" >> "$SCRIPT_FILE_PATH"
-    echo "  deploy_cloudformation_stack \$STACK_NAME \$TEMPLATE_FILE_PATH \"\" \$IAM_CAPABILITY " >> "$SCRIPT_FILE_PATH"
+    echo "  deploy_cloudformation_stack \$STACK_NAME \$TEMPLATE_FILE_PATH \$ENV_PROFILE \$REGION \"\" \$IAM_CAPABILITY " >> "$SCRIPT_FILE_PATH"
     echo "else" >> "$SCRIPT_FILE_PATH"
-    echo "  deploy_cloudformation_stack \$STACK_NAME \$TEMPLATE_FILE_PATH \$ENCODED_PARAMETERS \$IAM_CAPABILITY " >> "$SCRIPT_FILE_PATH"
+    echo "  deploy_cloudformation_stack \$STACK_NAME \$TEMPLATE_FILE_PATH \$ENV_PROFILE \$REGION \$ENCODED_PARAMETERS \$IAM_CAPABILITY " >> "$SCRIPT_FILE_PATH"
     echo "fi" >> "$SCRIPT_FILE_PATH"
    
     echo "Created deployment script at $SCRIPT_FILE_PATH"
