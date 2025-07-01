768f1483d79cfbd0f818286045584a18f2fdc65f
Teri Radichel <github@pwn.fyi>
2025-07-01 15:09:18 -0400

    Update get_env_param_value.sh

--- functions/get_env_param_value.sh
+++ functions/get_env_param_value.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
+#!/bin/bash -e
 get_env_param_value(){
   env_file_path="$1"
-  param="$2"
+  param_name="$2"
 
-  val=$(cat $env_file_path | grep $param | cut -d "=" -f2 | xargs)
+  if [ "$env_file_path" == "" ] || [ "$param" == "" ]; then
+     echo "Error: env_file_path and param_name must be set."
+     exit 1
+  fi
+  
+  val=$(cat $env_file_path | grep $param_name | cut -d "=" -f2 | xargs)
 
   echo $val
 }
