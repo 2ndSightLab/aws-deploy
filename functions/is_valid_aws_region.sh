bea0e423993f1dbfeaad96e9cb7bb0abb9cf9f73
Teri Radichel <github@pwn.fyi>
2025-07-01 16:42:47 -0400

    Update is_valid_aws_region.sh

--- functions/is_valid_aws_region.sh
+++ functions/is_valid_aws_region.sh
@@ -14,7 +14,7 @@ is_valid_aws_region() {
     fi
     
     # Fetch the list of valid AWS regions using us-east-1 so we know we're starting with a valid region
-    # If you use an invalid region DNS requests are made to whatever you specify. Which is not good...
+    # Presuming here the user has access to us-east-1. If you change this it should be hard-coded to a valid region.
     local aws_regions=$(aws ec2 describe-regions --query 'Regions[].RegionName' --region us-east-1 --output text 2>/dev/null)
     
     # Check if aws command failed
