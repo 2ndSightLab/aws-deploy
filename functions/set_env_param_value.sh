8703562176693ed269dd03a667bbf44fe7c5ec6c
Teri Radichel <github@pwn.fyi>
2025-07-01 23:18:21 -0400

    Update set_env_param_value.sh

--- functions/set_env_param_value.sh
+++ functions/set_env_param_value.sh
@@ -6,9 +6,16 @@ set_env_param_value() {
   env_file_path="$1"
   param_name="$2"
   value="$3"
-
-  #if [[ "$param_name" == *"|"* ]] && { echo "Error: The variable 'param_name' contains a pipe character '|', which is not allowed." >&2; exit 1; }  
-  #if [[ "$value" == *"|"* ]] && { echo "Error: The variable 'value' contains a pipe character '|', which is not allowed." >&2; exit 1; }
+    
+  if [[ "$param_name" == *"|"* ]]; then
+    echo "Error: The variable 'param_name' contains a pipe character '|', which is not allowed." >&2
+    exit 1
+  fi
+  
+  if [[ "$value" == *"|"* ]]; then
+    echo "Error: The variable 'value' contains a pipe character '|', which is not allowed." >&2
+    exit 1
+  fi
 
   #set the parameter value
   sed -i "s|^${param_name}=.*|${param_name}=\"${value}\"|" "$env_file_path"
