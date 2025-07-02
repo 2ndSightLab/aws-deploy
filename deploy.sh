937865ac8795114a85b3e31dec0b73c0522ec3e4
Teri Radichel <github@pwn.fyi>
2025-07-02 06:10:11 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -227,8 +227,11 @@ echo "Initializing...please wait..."
 STACK_NAME=$(get_stack_name "$ENV_NAME" "$IDENTITY_NAME" "$SERVICE_NAME" "$RESOURCE_NAME" "$NAME")
 STACK_RESOURCE_NAME=$(get_stack_resource_name "$ENV_NAME" "$SERVICE_NAME" "$RESOURCE_NAME" "$REGION" "$NAME")
 
-TEMPLATE_FILE_PATH=$(get_template_file_path $SERVICE_NAME $RESOURCE_NAME $GIT_REPO_DIR)
-create_cloudformation_template $SERVICE_NAME $RESOURCE_NAME $ENV_PROFILE $REGION
+ACCOUNT=$(get_aws_account $PROFILE)
+if [ -z "$ACCOUNT" ]; then echo "Error: Account not set from profile $ENV_PROFILE"; exit; fi
+
+TEMPLATE_FILE_PATH=$(get_template_file_path $SERVICE_NAME $RESOURCE_NAME $ACCOUNT $REGION $GIT_REPO_DIR)
+create_cloudformation_template $SERVICE_NAME $RESOURCE_NAME $TEMPLATE_FILE_PATH $ENV_PROFILE $REGION
 if [ ! -f $TEMPLATE_FILE_PATH ]; then echo "$TEMPLATE_FILE_PATH does not exist. Exiting."; exit; fi
 
 SCRIPT_FILE_PATH=$(get_script_file_path $SERVICE_NAME $RESOURCE_NAME)
