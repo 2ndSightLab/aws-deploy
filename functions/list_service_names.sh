9f805a4724ef1c4f2aec97c161b4cea238f95612
Teri Radichel <github@pwn.fyi>
2025-07-01 22:56:02 -0400

    Update list_service_names.sh

--- functions/list_service_names.sh
+++ functions/list_service_names.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 list_service_names(){
+
+  validate_fist_n_args_set 2
+
   local ENV_PROFILE=$1
   local REGION=$2
-  
-  if [ -z "$ENV_PROFILE" ]; then echo "$ENV_PROFILE not set in list_service_names" >&2; exit 1; fi
-  if [ -z "$REGION" ]; then echo "$REGION not set in list_service_names" >&2; exit 1; fi
   
   aws cloudformation list-types --visibility PUBLIC --type RESOURCE --profile $ENV_PROFILE --region $REGION --output json | jq -r '.TypeSummaries[].TypeName' | grep -o 'AWS::[^:]*' | sed 's/AWS:://' | sort -u
 }
