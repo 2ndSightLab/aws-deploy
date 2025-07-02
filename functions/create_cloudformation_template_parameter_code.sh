f7b170587fbc1c7d6f197304bd7dfef796346423
Teri Radichel <github@pwn.fyi>
2025-07-02 15:04:46 -0400

    Update create_cloudformation_template_parameter_code.sh

--- functions/create_cloudformation_template_parameter_code.sh
+++ functions/create_cloudformation_template_parameter_code.sh
@@ -8,9 +8,9 @@ create_cloudformation_template_parameter_code(){
     local SCHEMA_B64=$3
     
     local SCHEMA=$(echo "$SCHEMA_B64" | base64 -d)
-    if [ -z $SCHEMA ]; then echo "Error: $SCHEMA is empty generating template parameter code"; fi
+    if [ -z $SCHEMA ]; then echo "Error: $SCHEMA is empty generating template parameter code"; exit; fi
   
-    echo "Create cloud formation template parameter code"
+    echo "Create CloudFormation template parameter code"
 
     local properties_json=$(jq -r 'if type == "string" then fromjson else . end | .properties' <<< "$SCHEMA") 
     local readOnlyProps=$(jq -r 'if type == "string" then fromjson else . end | if has("readOnlyProperties") then .readOnlyProperties[] else empty end' <<< "$SCHEMA" | sed 's|/properties/||g')
