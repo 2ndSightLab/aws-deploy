d075880172dde6be09754718e2fe217a7245f3e1
Teri Radichel <github@pwn.fyi>
2025-07-02 06:53:49 -0400

    Update script_file.sh

--- init/script_file.sh
+++ init/script_file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 SCRIPT_FILE_PATH=$(get_script_file_path $SERVICE_NAME $RESOURCE_NAME)
-create_deploy_script_for_resource $RESOURCE_TYPE $SCHEMA_B64 $ENV_PROFILE $REGION
+create_deploy_script_for_resource $RESOURCE_TYPE $SCHEMA_B64 $SCRIPT_FILE_PATH $TEMPLATE_FILE_PATH $ENV_PROFILE $REGION
 if [ ! -f $SCRIPT_FILE_PATH ]; then echo "$SCRIPT_FILE_PATH does not exist. Exiting."; exit; fi
