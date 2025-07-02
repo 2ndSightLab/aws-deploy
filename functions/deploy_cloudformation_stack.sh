93d78f658ed9cff7d228976f9ae4ef2539112610
Teri Radichel <github@pwn.fyi>
2025-07-01 22:50:39 -0400

    Update deploy_cloudformation_stack.sh

--- functions/deploy_cloudformation_stack.sh
+++ functions/deploy_cloudformation_stack.sh
@@ -1,17 +1,14 @@
 #/bin/bash -e
 deploy_cloudformation_stack() {
+  
+    validate_fist_n_args_set 5
+    
     local STACK_NAME=$1
     local TEMPLATE_FILE_PATH=$2
     local ENCODED_PARAMETER_LIST=$3
     local ENV_PROFILE=$4
     local REGION=$5
     local IAM=${6:-false}
-
-    if [ -z "$STACK_NAME" ]; then echo "$STACK_NAME not set in deploy_cloudformation_stack" >&2; exit 1; fi
-    if [ -z "$TEMPLATE_FILE_PATH" ]; then echo "$TEMPLATE_FILE_PATH not set in deploy_cloudformation_stack" >&2; exit 1; fi    
-    if [ -z "$ENCODED_PARAMETER_LIST" ]; then echo "$ENCODED_PARAMETER_LIST not set in deploy_cloudformation_stack" >&2; exit 1; fi
-    if [ -z "$ENV_PROFILE" ]; then echo "$ENV_PROFILE not set in deploy_cloudformation_stack" >&2; exit 1; fi
-    if [ -z "$REGION" ]; then echo "$REGION not set in deploy_cloudformation_stack" >&2; exit 1; fi
     
     # Check if the stack exists in a failed state
     if aws cloudformation describe-stacks --stack-name "$STACK_NAME" --profile $ENV_PROFILE --region $REGION 2>/dev/null | grep -q "CREATE_FAILED\|ROLLBACK_COMPLETE"; then
