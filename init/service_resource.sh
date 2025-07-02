d635686be6dcb76d03f9853c4226f8bd52edd439
Teri Radichel <github@pwn.fyi>
2025-07-02 12:38:51 -0400

    Update service_resource.sh

--- init/service_resource.sh
+++ init/service_resource.sh
@@ -1,15 +1,30 @@
 #!/bin/bash -e
 
-prompt_resource="
-Enter the resource of the service $RESOURCE_NAME that you want to deploy (type help for a list of resources):
+if [ -n "RESOURCE_NAME" ]; then 
+
+prompt_service="
+The current $SERVICE_NAME resource name is $RESOURCE_NAME. Enter a new service name to change it. (type help for a list of services):
 "
-RESOURCE_NAME=""
+
+else
+
+prompt_service="
+Enter the resource of the $SERVICE_NAME resource name that you want to deploy (type help for a list of resources):
+"
+
+fi
+
 while [ -z "$RESOURCE_NAME" ]; do
-    read -p "$prompt_resource" RESOURCE_NAME
-    if [ "$RESOURCE_NAME" == "help" ]; then
-       list_service_resource_names $SERVICE_NAME $ENV_PROFILE $REGION
-       RESOURCE_NAME=""
+    read -p "$prompt_service" CHANGE_SERVICE_NAME
+    if [ "$CHANGE_RESOURCE_NAME" == "help" ]; then
+      list_service_resource_names $SERVICE_NAME $ENV_PROFILE $REGION
+      CHANGE_RESOURCE_NAME=""
+    fi
+    #use existing service
+    if [ -n "$RESOURCE_NAME" ] && [ -z "$CHANGE_RESOURCE_NAME" ]; then break; fi
+    #use new service name
+    if [ -n "$CHANGE_RESOURCE_NAME" ]; then 
+        RESOURCE_NAME="$CHANGE_RESOURCE_NAME";  
+       is_valid_service_resource $SERVICE_NAME $RESOURCE_NAME $ENV_PROFILE $REGION
     fi
 done
-
-is_valid_service_resource $SERVICE_NAME $RESOURCE_NAME $ENV_PROFILE $REGION
