d518e5f72d2ec61d732428622df2a485ff99f858
Teri Radichel <github@pwn.fyi>
2025-07-02 06:16:12 -0400

    Update get_aws_account.sh

--- functions/get_aws_account.sh
+++ functions/get_aws_account.sh
@@ -3,8 +3,8 @@ get_aws_account(){
 
   validate_first_n_args_set 2  "$@"
 
-  local ENV_PROFILE="$3"
-  local REGION="$4"
+  local ENV_PROFILE="$1"
+  local REGION="$2"
 
   echo $(aws sts get-caller-identity --query "Account" --profile $ENV_PROFILE --region $REGION --output text)
   
