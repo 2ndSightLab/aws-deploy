60682ed2b01bc1b2b1b5d6cbbd134b8b2549ef44
Teri Radichel <github@pwn.fyi>
2025-07-02 12:18:05 -0400

    Update identity.sh

--- init/identity.sh
+++ init/identity.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-IDENTITY_ARN=$(get_current_identity_arn $ENV_PROFILE $REGION)
-IDENTITY_NAME=$(get_identity_name_from_arn $IDENTITY_ARN $ENV_PROFILE $REGION)
+IDENTITY_ARN=$(get_current_identity_arn $ENV_PROFILE)
+IDENTITY_NAME=$(get_identity_name_from_arn $IDENTITY_ARN $ENV_PROFILE)
