2229b6f002be535132660eab631a6964838edbff
Teri Radichel <github@pwn.fyi>
2025-07-02 18:46:25 -0400

    Update create_deploy_script_resource_properties.sh

--- functions/create_deploy_script_resource_properties.sh
+++ functions/create_deploy_script_resource_properties.sh
@@ -96,7 +96,7 @@ create_deploy_script_resource_properties() {
 
                 # Add to parameter overrides
                 echo "if [[ -n \"\${${property}_value}\" ]]; then" >> "$SCRIPT_FILE_PATH"
-                echo "  Property Value: \"${${property}_value}\"" >> "$SCRIPT_FILE_PATH"
+                echo "  Property Value: \"\${${property}_value}\"" >> "$SCRIPT_FILE_PATH"
                 echo "  # Handle special characters including @ in parameter values" >> "$SCRIPT_FILE_PATH"
                 echo "  SAFE_VALUE=\$(printf '%q' \"\${${property}_value}\")" >> "$SCRIPT_FILE_PATH"
                 echo "  SAFE_VALUE: $SAFE_VALUE" >> "$SCRIPT_FILE_PATH"
