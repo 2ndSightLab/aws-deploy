fde85e095ddc4c2a3ed5a5d1d5d0f94c08153a72
Teri Radichel <github@pwn.fyi>
2025-07-02 05:12:03 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -178,7 +178,7 @@ echo "ENV_PROFILE: $ENV_PROFILE"
 
 #set the region
 echo "get profile region"
-REGION=$(get_region)
+REGION=$(get_region $ENV_PROFILE)
 echo "The current region is $REGION. If you want to change the region enter it now"
 read CHANGE_REGION
 if [ "$CHANGE_REGION" != "" ]; then REGION=$CHANGE_REGION; fi
