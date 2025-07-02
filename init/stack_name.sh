f855f753d5cd20241e55ec164d09bbb7c4ad613e
Teri Radichel <github@pwn.fyi>
2025-07-02 13:28:28 -0400

    Update stack_name.sh

--- init/stack_name.sh
+++ init/stack_name.sh
@@ -1,7 +1,10 @@
+#!/bin/bash -e
+
+echo "generating stack name"
+
 prompt_name="
 If the resource is a user, for a specific user, or associated with an application enter the name. Enter if n/a:
 "
 read -p "$prompt_name" NAME
 
-echo "generating stack name"
 STACK_NAME=$(get_stack_name "$ENV_NAME" "$IDENTITY_NAME" "$SERVICE_NAME" "$RESOURCE_NAME" "$NAME")
