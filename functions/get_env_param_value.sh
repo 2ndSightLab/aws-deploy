db52746279ec007f1919cfaf99ec9cef012429be
Teri Radichel <github@pwn.fyi>
2025-07-01 21:41:12 -0400

    Update get_env_param_value.sh

--- functions/get_env_param_value.sh
+++ functions/get_env_param_value.sh
@@ -6,9 +6,7 @@ get_env_param_value(){
   if [ -z "$env_file_path" ]; then echo "$env_file_path not set in get_env_param_value" >&2; exit 1; fi
   if [ -z "$param_name" ]; then echo "$param_name not set in get_env_param_value" >&2; exit 1; fi
 
-  if [ -f "$env_file_path" ]; then 
-      echo ""; return 
-  fi
+  if [ -f "$env_file_path" ]; then echo ""; return fi
 
   val=$(grep "$param_name:" "$env_file_path" | sed -e 's/^.*://' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
