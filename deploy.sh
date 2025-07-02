a14157a4dd7db4cd82a62b8d3df3396c0294346d
Teri Radichel <github@pwn.fyi>
2025-07-02 05:11:21 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -177,16 +177,11 @@ fi
 echo "ENV_PROFILE: $ENV_PROFILE"
 
 #set the region
+echo "get profile region"
 REGION=$(get_region)
 echo "The current region is $REGION. If you want to change the region enter it now"
 read CHANGE_REGION
 if [ "$CHANGE_REGION" != "" ]; then REGION=$CHANGE_REGION; fi
-#is_valid_aws_region $REGION
-
-#if [ $? -ne 0 ]; then
-#    echo "Error: Not a valid region $REGION."
-#    exit 1
-#fi
 
 echo "testing profile"
 aws sts get-caller-identity --profile $ENV_PROFILE --region $REGION
