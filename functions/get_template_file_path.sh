f53b4557d3dd0aa529311f37bb2dd4c4f63bfb9e
Teri Radichel <github@pwn.fyi>
2025-07-02 05:55:36 -0400

    Update and rename get_template_file_name.sh to get_template_file_path.sh

--- functions/get_template_file_path.sh
+++ functions/get_template_file_path.sh
@@ -4,7 +4,8 @@ get_template_file_path(){
     validate_first_n_args_set 2  "$@"
     
     local SERVICE_NAME="$1"
-    local RESOURCE_NAME="$2" 
+    local RESOURCE_NAME="$2"
+    local GIT_REPO_DIR="$3" 
 
     local DIR_PATH="resources/$SERVICE_NAME"
     
@@ -12,7 +13,10 @@ get_template_file_path(){
     if [ ! -d "$DIR_PATH" ]; then
         mkdir -p "$DIR_PATH"
     fi
-    
-    echo "$DIR_PATH/$RESOURCE_NAME.yaml"
+
+    path="$DIR_PATH/$RESOURCE_NAME.yaml"
+    if [ -z "$GIT_REPO_DIR" ]; then 
+      path="$GIT_REPO_DIR/$path"
+    fi
     
 }
