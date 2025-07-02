ec365a6cdf6647cc2bc76f527675e35a337179f6
Teri Radichel <github@pwn.fyi>
2025-07-02 13:58:49 -0400

    Update stack_resource_name.sh

--- init/stack_resource_name.sh
+++ init/stack_resource_name.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
-
+echo "Generating STACK_RESOURCE_NAME"
 STACK_RESOURCE_NAME=$(get_stack_resource_name "$ENV_NAME" "$SERVICE_NAME" "$RESOURCE_NAME" "$REGION" "$NAME")
+echo "STACK_RESOURCE_NAME: $STACK_RESOURCE_NAME"
