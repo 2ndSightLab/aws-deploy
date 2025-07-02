d4afe4ea2ee929e3985a1c0846e45a3022c82947
Teri Radichel <github@pwn.fyi>
2025-07-02 14:18:45 -0400

    Update script_file.sh

--- init/script_file.sh
+++ init/script_file.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -e
-SCRIPT_FILE_PATH=$(get_script_file_path $SERVICE_NAME $RESOURCE_NAME)
-create_deploy_script_for_resource $RESOURCE_TYPE $SCHEMA_B64 $SCRIPT_FILE_PATH $TEMPLATE_FILE_PATH $ENV_PROFILE $REGION
+echo "Generating script file path
+SCRIPT_FILE_PATH=$(get_script_file_path $SERVICE_NAME $RESOURCE_NAME $ACCOUNT $REGION $GIT_REPO_DIR)
+echo "SCRIPT_FILE_PATH: $SCRIPT_FILE_PATH"
+echo "Creating deploy script for resource: $RESOURCE_TYPE and temlpate file: $TEMPLATE_FILE_PATH in region: $REGION"
+create_deploy_script_for_resource $RESOURCE_TYPE $SCRIPT_FILE_PATH $TEMPLATE_FILE_PATH $ENV_PROFILE $REGION $SCHEMA_B64
 if [ ! -f $SCRIPT_FILE_PATH ]; then echo "$SCRIPT_FILE_PATH does not exist. Exiting."; exit; fi
