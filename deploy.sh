ee4f32b9cb63c808475532ab6faf9697220b2665
Teri Radichel <github@pwn.fyi>
2025-07-02 12:25:02 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -16,60 +16,69 @@ run(){
   local GIT_REPO_NAME=""
   local ENV_PROFILE=""
   local ACCOUNT=""
-
+  
   source init/environment.sh
 
   source init/git_repo.sh
 
   source init/aws_profile.sh
 
-  source init/region.sh
-
   source init/identity.sh
 
-  source init/service.sh
+  while [ true ]; do
 
-  source init/service_resource.sh
-
-  source init/stack_name.sh
-
-  source init/stack_resource_name.sh
-
-  source init/aws_account_number.sh
-
-  source init/service_resource_schema.sh
+    source init/region.sh
   
-  source init/template_file.sh
+    source init/service.sh
+  
+    source init/service_resource.sh
+  
+    source init/stack_name.sh
+  
+    source init/stack_resource_name.sh
+  
+    source init/aws_account_number.sh
+  
+    source init/service_resource_schema.sh
+    
+    source init/template_file.sh
+  
+    source init/script_file.sh
 
-  source init/script_file.sh
+    echo ""
+    echo "ENV: $ENV_NAME"
+    echo "IDENTITY_ARN: $IDENTITY_ARN"
+    echo "IDENTITY_NAME: $IDENTITY_NAME"
+    echo "REGION: $REGION"
+    echo "STACK_NAME: $STACK_NAME"
+    echo "STACK_RESOURCE_NAME: $STACK_RESOURCE_NAME"
+    echo "TEMPLATE FILE: $TEMPLATE_FILE_PATH"
+    echo "SCRIPT: $SCRIPT_FILE_PATH" 
+    echo "AWS CLI PROFILE: $ENV_PROFILE"
+    echo "RESOURCE_TYPE: $RESOURCE_TYPE"
+    echo "GIT_REPO_URL: $GIT_REPO_URL"
+    echo "ENV_PROFILE: $ENV_PROFILE"
+    echo "REGION: $REGION"
+    echo "ACCOUNT: $ACCOUNT"
+    echo "TEMPLATE FILE PATH: $TEMPLATE_FILE_PATH"
+    echo "SCRIPT_FILE_PATH: $SCRIPT_FILE_PATH"
+    echo ""
+  
+    #the deploy script assumes the above values have been set prior to sourcing it
+    echo "Enter to deploy the resource. Control-C to exit."
+    read ok
+  
+    echo "Execute the deploy script $SCRIPT_FILE_PATH"
+   
+    # this script depends on some of the variables set above
+    source $SCRIPT_FILE_PATH
 
-  echo ""
-  echo "ENV: $ENV_NAME"
-  echo "IDENTITY_ARN: $IDENTITY_ARN"
-  echo "IDENTITY_NAME: $IDENTITY_NAME"
-  echo "REGION: $REGION"
-  echo "STACK_NAME: $STACK_NAME"
-  echo "STACK_RESOURCE_NAME: $STACK_RESOURCE_NAME"
-  echo "TEMPLATE FILE: $TEMPLATE_FILE_PATH"
-  echo "SCRIPT: $SCRIPT_FILE_PATH" 
-  echo "AWS CLI PROFILE: $ENV_PROFILE"
-  echo "RESOURCE_TYPE: $RESOURCE_TYPE"
-  echo "GIT_REPO_URL: $GIT_REPO_URL"
-  echo "ENV_PROFILE: $ENV_PROFILE"
-  echo "REGION: $REGION"
-  echo "ACCOUNT: $ACCOUNT"
-  echo "TEMPLATE FILE PATH: $TEMPLATE_FILE_PATH"
-  echo "SCRIPT_FILE_PATH: $SCRIPT_FILE_PATH"
-  echo ""
+    echo "Do you want to deploy another resource? Enter to continue. Ctrl-C to exit."
+    read ok
+    
+  done
 
-  #the deploy script assumes the above values have been set prior to sourcing it
-  echo "Enter to deploy the resource. Control-C to exit."
-  read ok
-
-  echo "Execute the deploy script $SCRIPT_FILE_PATH"
-
-  # this script depends on some of the variables set above
-  source $SCRIPT_FILE_PATH
+ 
 }
 
 #run in function to protect local vars
