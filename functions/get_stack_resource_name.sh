4ece3ab3c7bf597928c0ec55c68c1e404ccf067d
Teri Radichel <github@pwn.fyi>
2025-07-02 13:25:08 -0400

    Update get_stack_resource_name.sh

--- functions/get_stack_resource_name.sh
+++ functions/get_stack_resource_name.sh
@@ -9,16 +9,14 @@ get_stack_resource_name() {
     local REGION="$4"
     local NAME="$5" #optional
     
-
-    # Return the concatenated string
+    echo "generating stack resource name"
+    
     resource_name="$ENV_NAME-$SERVICE-$RESOURCE"
 
     if [ "$NAME" != "" ]; then
         resource_name="$resource_name-$NAME"
     fi
 
-    #adding region so it is clear which region the command was executed in to create this resource (for global resources)
-    #and to create a unique name per region if neccessary
     resource_name=$resource_name-$REGION
     
     echo $resource_name | tr '[:upper:]' '[:lower:]'
