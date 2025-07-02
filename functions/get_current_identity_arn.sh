4811d5f5b53b6406de7c0c486b71031666c0b12b
Teri Radichel <github@pwn.fyi>
2025-07-02 12:48:46 -0400

    Update get_current_identity_arn.sh

--- functions/get_current_identity_arn.sh
+++ functions/get_current_identity_arn.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
 get_current_identity_arn() {
 
-    validate_first_n_args_set 2  "$@"
+    validate_first_n_args_set 1  "$@"
     
     local ENV_PROFILE="$1"
-    local REGION="$2"
     
     local arn
     arn=$(aws sts get-caller-identity --query 'Arn' --profile $ENV_PROFILE --output text)
