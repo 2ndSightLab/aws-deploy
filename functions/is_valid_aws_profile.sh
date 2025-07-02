b906fb26714ec466aedbd383542e8521a0bcf4c7
Teri Radichel <github@pwn.fyi>
2025-07-02 04:56:59 -0400

    Update is_valid_aws_profile.sh

--- functions/is_valid_aws_profile.sh
+++ functions/is_valid_aws_profile.sh
@@ -1,31 +1,31 @@
 #!/bin/bash -e
 is_valid_aws_profile() {
-     
-    validate_first_n_args_set 1  "$@"
-     
-    local profile="$1"
-    
-    # if profile doesn't match an existing profile
-    if aws configure list-profiles 2>/dev/null | grep -q "^${profile}$"; then
 
-        if [ -n "$AWS_EXECUTION_ENV" ] && [ "$profile" == "default" ]; then
+    local profile="$1"
+    local REGION="$2"
+
+    exists="n"
+    if aws configure list-profiles | grep -q "^${profile}$"; then
+        exists="y"
+    else
+
+        if [ -n "$AWS_EXECUTION_ENV" ] && [ "$profile" == "default" ];  then
+
+          echo "AWS CloudShell environment" >&2
 
           #add default profile
           echo "[default]" >> ~/.aws/config
           echo "region = $REGION" >> ~/.aws/config
           echo "output = json" >> ~/.aws/config
           echo "credential_source = EcsContainer" >> ~/.aws/config
-          
-          if aws configure list-profiles 2>/dev/null | grep -q "^${profile}$"; then
-             #profile exists
-             return 0
-          else
-             return 1
+
+          if aws configure list-profiles | grep -q "^${profile}$"; then
+             echo "Default profile added for CloudShell"
+             exists="y"
           fi
-        
-        fi
-        return 0
-    else
-        return 1
+       fi
     fi
+
+    if [ "$exists" == "y" ]; then return 0; else return 1; fi
+
 }
