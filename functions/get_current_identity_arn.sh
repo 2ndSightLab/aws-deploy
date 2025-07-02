6e17d25313790d5b6d7a583205f42fe2c2ba5f48
Teri Radichel <github@pwn.fyi>
2025-07-02 05:19:25 -0400

    Update get_current_identity_arn.sh

--- functions/get_current_identity_arn.sh
+++ functions/get_current_identity_arn.sh
@@ -8,10 +8,6 @@ get_current_identity_arn() {
     
     local arn
     arn=$(aws sts get-caller-identity --query 'Arn' --profile $ENV_PROFILE --region $REGION --output text)
-    if [ $? -ne 0 ]; then
-        echo "Error: Failed to retrieve identity ARN. Check your AWS credentials and permissions." >&2
-        exit 1
-    fi
     
     echo "$arn"
 }
