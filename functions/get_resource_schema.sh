4c67460d0ee65ce1868b31c23a39b7b12653c5df
Teri Radichel <github@pwn.fyi>
2025-07-01 22:52:23 -0400

    Update get_resource_schema.sh

--- functions/get_resource_schema.sh
+++ functions/get_resource_schema.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -e
 #resource_type: "AWS::$SERVICE_NAME::$RESOURCE_NAME"
 get_resource_schema(){
+
+  validate_fist_n_args_set 3
+
   local resource_type="$1"
   local ENV_PROFILE=$2
   local REGION=$3
-
-  if [ -z "$resource_type" ]; then echo "$resource_type not set in get_resource_schema" >&2; exit 1; fi
-  if [ -z "$ENV_PROFILE" ]; then echo "$ENV_PROFILE not set in get_resource_schema" >&2; exit 1; fi
-  if [ -z "$REGION" ]; then echo "$REGION not set in get_resource_schema" >&2; exit 1; fi
 
   schema=$(aws cloudformation describe-type --type RESOURCE --type-name "$resource_type" --query 'Schema' --profile $ENV_PROFILE --region $REGION --output json)
   echo $schema
