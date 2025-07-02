da4a1e138fe4737716336ef6ba3a42004021eb43
Teri Radichel <github@pwn.fyi>
2025-07-02 13:28:05 -0400

    Update service_resource_schema.sh

--- init/service_resource_schema.sh
+++ init/service_resource_schema.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+echo "Get service resource schema"
 RESOURCE_TYPE="AWS::$SERVICE_NAME::$RESOURCE_NAME"
+
 SCHEMA=$(get_resource_schema $RESOURCE_TYPE $ENV_PROFILE $REGION)
+if [ -z "$SCHEMA" ]; then echo "Error: No schmea returned for resource type: $RESOURCE_TYPE"; exit 1; fi
+
 SCHEMA_B64=$(echo "$SCHEMA" | base64)
+
