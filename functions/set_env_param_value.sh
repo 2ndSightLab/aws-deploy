ff1d4ead3476a0a54a34cfc32140a3a9b28c7a58
Teri Radichel <github@pwn.fyi>
2025-07-01 22:56:28 -0400

    Update set_env_param_value.sh

--- functions/set_env_param_value.sh
+++ functions/set_env_param_value.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set_env_param_value(){
+
+  validate_fist_n_args_set 3
+
   env_file_path="$1"
   param_name="$2"
   value="$3"
 
-  if [ -z "$env_file_path" ]; then echo "$env_file_path not set in set_env_param_value" >&2; exit 1; fi 
-  if [ -z "$param_name" ]; then echo "$param_name not set in set_env_param_value" >&2; exit 1; fi
-  if [ -z "$value" ]; then echo "$value not set in set_env_param_value" >&2; exit 1; fi 
   if [[ "$param_name" == *"|"* ]] && { echo "Error: The variable 'param_name' contains a pipe character '|', which is not allowed." >&2; exit 1; }  
   if [[ "$value" == *"|"* ]] && { echo "Error: The variable 'value' contains a pipe character '|', which is not allowed." >&2; exit 1; }
 
