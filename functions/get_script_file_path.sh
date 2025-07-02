d83c6e7b1e7a9d1e9dafe283144f4f79196be839
Teri Radichel <github@pwn.fyi>
2025-07-01 22:52:46 -0400

    Update get_script_file_path.sh

--- functions/get_script_file_path.sh
+++ functions/get_script_file_path.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -e
 get_script_file_path(){
+
+    validate_fist_n_args_set 2
+    
     local SERVICE_NAME="$1"
     local RESOURCE_NAME="$2" 
 
-    if [ -z "$SERVICE_NAME" ]; then echo "$SERVICE_NAME not set in get_script_file_path" >&2; exit 1; fi
-    if [ -z "$RESOURCE_NAME" ]; then echo "$RESOURCE_NAME not set in get_script_file_path" >&2; exit 1; fi
-    
     local DIR_PATH="resources/$SERVICE_NAME"
     
     # Create the directory structure if it doesn't exist
