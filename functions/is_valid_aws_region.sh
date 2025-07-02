14103308c0809a0ffcbd26df6cd97eac7d59ec6c
Teri Radichel <github@pwn.fyi>
2025-07-02 13:32:49 -0400

    Update is_valid_aws_region.sh

--- functions/is_valid_aws_region.sh
+++ functions/is_valid_aws_region.sh
@@ -5,7 +5,9 @@ is_valid_aws_region() {
     
     local REGION=$1
     local ENV_PROFILE=$2
-   
+
+    echo "Checking to see if $REGION" is a valid region."
+    
     # Fetch the list of valid AWS regions using us-east-1 so we know we're starting with a valid region
     # Presuming here the user has access to us-east-1. If you change this it should be hard-coded to a valid region.
     local aws_regions=$(aws ec2 describe-regions --query 'Regions[].RegionName' --profile $ENV_PROFILE --region us-east-1 --output text 2>/dev/null)
