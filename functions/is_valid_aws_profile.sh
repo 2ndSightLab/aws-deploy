3ef59847de0c3949d104ae0530a1c4df4795ef3a
Teri Radichel <github@pwn.fyi>
2025-07-02 03:54:18 -0400

    Update is_valid_aws_profile.sh

--- functions/is_valid_aws_profile.sh
+++ functions/is_valid_aws_profile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 is_valid_aws_profile() {
      
-     validate_first_n_args_set 1  "$@"
+    validate_first_n_args_set 1  "$@"
      
     local profile="$1"
     
@@ -11,8 +11,25 @@ is_valid_aws_profile() {
         return 2
     fi
     
-    # Check if the profile exists in AWS config files
+    # if profile doesn't match an existing profile
     if aws configure list-profiles 2>/dev/null | grep -q "^${profile}$"; then
+
+        if [ -n "$AWS_EXECUTION_ENV" ]; then
+
+          #add default profile
+          echo "[default]" >> ~/.aws/config
+          echo "region = $REGION" >> ~/.aws/config
+          echo "output = json" >> ~/.aws/config
+          echo "credential_source = EcsContainer" >> ~/.aws/config
+          
+          if aws configure list-profiles 2>/dev/null | grep -q "^${profile}$"; then
+             #profile exists
+             return 0
+          else
+             return 1
+          fi
+        
+        fi
         return 0
     else
         return 1
