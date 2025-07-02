476322ac4ca23de2386be9c1fa90718399a01b78
Teri Radichel <github@pwn.fyi>
2025-07-02 06:57:39 -0400

    Update template_file.sh

--- init/template_file.sh
+++ init/template_file.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 TEMPLATE_FILE_PATH=$(get_template_file_path $SERVICE_NAME $RESOURCE_NAME $ACCOUNT $REGION $GIT_REPO_DIR)
-create_cloudformation_template $SERVICE_NAME $RESOURCE_NAME $TEMPLATE_FILE_PATH $ENV_PROFILE $REGION
+create_cloudformation_template $RESOURCE_NAME $RESOURCE_TYPE $SCHEMA_B64 $TEMPLATE_FILE_PATH $ENV_PROFILE $REGION
 if [ ! -f $TEMPLATE_FILE_PATH ]; then echo "$TEMPLATE_FILE_PATH does not exist. Exiting."; exit; fi
