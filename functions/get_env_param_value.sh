34324bd5e2be55a60f8880276fc41c47e7f58a30
Teri Radichel <github@pwn.fyi>
2025-07-01 22:51:21 -0400

    Update get_env_param_value.sh

--- functions/get_env_param_value.sh
+++ functions/get_env_param_value.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 get_env_param_value(){
+
+  validate_fist_n_args_set 2
+  
   env_file_path="$1"
   param_name="$2"
-
-  if [ -z "$env_file_path" ]; then echo "$env_file_path not set in get_env_param_value" >&2; exit 1; fi
-  if [ -z "$param_name" ]; then echo "$param_name not set in get_env_param_value" >&2; exit 1; fi
 
   if [ ! -f "$env_file_path" ]; then echo ""; return; fi
 
