37e6ac9c0806b3c6a3cfc10ca420dc7b2e17cc43
Teri Radichel <github@pwn.fyi>
2025-07-01 21:38:23 -0400

    Update get_env_param_value.sh

--- functions/get_env_param_value.sh
+++ functions/get_env_param_value.sh
@@ -6,15 +6,16 @@ get_env_param_value(){
   if [ -z "$env_file_path" ]; then echo "$env_file_path not set in get_env_param_value" >&2; exit 1; fi
   if [ -z "$param_name" ]; then echo "$param_name not set in get_env_param_value" >&2; exit 1; fi
 
-  if [ -f "$env_file_path" ]; then echo ""; return; fi 
-  
-  val=$(grep "$param_name:" "$env_file_path" | sed -e 's/^.*://' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
-
-  # Check if the sed command was successful
-  if [ $? -ne 0 ]; then
-     echo "Error: Failed to get parameter '$param_name' in file '$env_file_path'." >&2
-     exit 1
+  if [ -f "$env_file_path" ]; then 
+      echo ""; return 
   fi
 
+  val=$(grep "$param_name:" "$env_file_path" | sed -e 's/^.*://' -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+
+  if [ $? -ne 0 ]; then
+       echo "Error: Failed to get parameter '$param_name' in file '$env_file_path'." >&2
+       exit 1
+  fi
+  
   echo $val
 }
