d5c07746bdc146f4a808b8a3d1dd79f68a55e8d9
Teri Radichel <github@pwn.fyi>
2025-07-02 12:26:34 -0400

    Update region.sh

--- init/region.sh
+++ init/region.sh
@@ -5,7 +5,7 @@ echo "get profile region"
 REGION=$(get_region $ENV_PROFILE)
 echo "The current region is $REGION. If you want to change the region enter it now"
 read CHANGE_REGION
-if [ "$CHANGE_REGION" != "" ]; then REGION=$CHANGE_REGION; fi
+if [ -n "$CHANGE_REGION"]; then REGION=$CHANGE_REGION; fi
 
 echo "testing profile"
 aws sts get-caller-identity --profile $ENV_PROFILE --region $REGION
