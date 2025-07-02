4c017f4e7ad108a13fff14d9f7af594e6ba78e65
Teri Radichel <github@pwn.fyi>
2025-07-02 07:05:18 -0400

    Update get_script_file_path.sh

--- functions/get_script_file_path.sh
+++ functions/get_script_file_path.sh
@@ -4,10 +4,17 @@ get_script_file_path(){
     validate_first_n_args_set 2  "$@"
     
     local SERVICE_NAME="$1"
-    local RESOURCE_NAME="$2" 
+    local RESOURCE_NAME="$2"
+    local ACCOUNT="$3"
+    local REGION="$4"
+    local GIT_REPO_DIR="$5" 
 
     local DIR_PATH="resources/$SERVICE_NAME"
     
+    if [ -n "$GIT_REPO_DIR" ]; then 
+        DIR_PATH="$GIT_REPO_DIR/$DIR_PATH"
+    fi
+
     # Create the directory structure if it doesn't exist
     if [ ! -d "$DIR_PATH" ]; then
         mkdir -p "$DIR_PATH"
