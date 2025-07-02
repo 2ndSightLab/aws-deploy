ea22b71987e7d75859f73067f0d6201faf9a2fbc
Teri Radichel <github@pwn.fyi>
2025-07-02 01:25:47 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -78,8 +78,9 @@ if [ -z "$GIT_REPO_URL" ]; then
   
 fi
 
-GIT_REPO_NAME=$(basename "$GIT_REPO_URL" | sed 's|\.git$||' | sed 's| ||g')
-echo "REPO_NAME: $GIT_REPO_NAME"
+GIT_REPO_NAME=$(basename "$GIT_REPO_URL")
+GIT_REPO_NAME=$(echo $GIT_REPO_URL | sed 's|\.git$||' | sed 's| ||g')
+echo "GIT_REPO_NAME: $GIT_REPO_NAME"
 if [ -z $GIT_REPO_NAME ]; then echo "GIT_REPO_NAME not set"; exit 1; fi
 
 prompt_git_parent_dir="
