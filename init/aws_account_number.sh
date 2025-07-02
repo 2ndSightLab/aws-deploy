b4b8386a9349e2c94da73b8a9ece9f6d188ed8ac
Teri Radichel <github@pwn.fyi>
2025-07-02 12:51:10 -0400

    Update aws_account_number.sh

--- init/aws_account_number.sh
+++ init/aws_account_number.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
-ACCOUNT=$(get_aws_account $ENV_PROFILE $REGION)
+ACCOUNT=$(get_aws_account $ENV_PROFILE)
 if [ -z "$ACCOUNT" ]; then echo "Error: Account not set from profile $ENV_PROFILE"; exit; fi
