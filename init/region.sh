b550978179a523a58128ef9982495bd5c9ec2324
Teri Radichel <github@pwn.fyi>
2025-07-03 13:46:52 -0400

    Update region.sh

--- init/region.sh
+++ init/region.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
 #set the region
-echo "get profile region"
+if [ $DEBUG ]; then 
+  echo "get profile region"
+fi 
 REGION=$(get_region $ENV_PROFILE)
 echo "The current region is $REGION. If you want to change the region enter it, otherwise enter."
 read CHANGE_REGION
