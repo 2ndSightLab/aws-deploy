da5699349331952bd54f1904a3027146da7219b7
Teri Radichel <github@pwn.fyi>
2025-07-03 13:46:02 -0400

    Update file_paths.sh

--- init/file_paths.sh
+++ init/file_paths.sh
@@ -1,6 +1,6 @@
 #!/bin/bash.sh -e
 
-if [ DEBUG ]; then echo "generating file paths"; fi
+if [ $DEBUG ]; then echo "generating file paths"; fi
 
 BASE_FILE_PATH="$ACCOUNT/$REGION/$SERVICE_NAME/$RESOURCE_NAME"
     
@@ -25,7 +25,7 @@ TEMPLATE_FILE_PATH="$RESOURCE_PATH/$RESOURCE_NAME.yaml"
 SCRIPT_FILE_PATH="$RESOURCE_PATH/$RESOURCE_NAME.sh"
 STACK_FILE_PATH="$STACKS_PATH/$STACK_NAME.cfg"
 
-if [ DEBUG ]; then 
+if [ $DEBUG ]; then 
     echo "TEMPLATE_FILE_PATH: $TEMPLATE_FILE_PATH"
     echo "SCRIPT_FILE_PATH: $SCRIPT_FILE_PATH"
     echo "STACK_FILE_PATH: $STACK_FILE_PATH"
