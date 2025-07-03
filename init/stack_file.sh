d0ffd59f31888d858dfd07f21a72768579ca1de1
Teri Radichel <github@pwn.fyi>
2025-07-02 20:40:25 -0400

    Update stack_file.sh

--- init/stack_file.sh
+++ init/stack_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "Generate stack file name"
-STACK_FILE_NAME=$(get_stack_file_path \
+STACK_FILE_PATH=$(get_stack_file_path \
     SERVICE_NAME \
     RESOURCE_NAME \
     STACK_NAME \
@@ -9,6 +9,6 @@ STACK_FILE_NAME=$(get_stack_file_path \
     REGION \
     GIT_REPO_DIR)
 
-if [ -z "$STACK_FILE_NAME" ]; then echo "Error: STACK_FILE_NAME not set"; exit; fi
+if [ -z "$STACK_FILE_PATH" ]; then echo "Error: STACK_FILE_NAME not set"; exit; fi
 
-echo "STACK_FILE_NAME: $STACK_FILE_NAME"
+echo "STACK_FILE_NAME: $STACK_FILE_PATH"
