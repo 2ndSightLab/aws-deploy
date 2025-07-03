d76a5d90f5d0771b29502d133a5b43b0780fc21f
Teri Radichel <github@pwn.fyi>
2025-07-03 13:35:29 -0400

    Update create_cloudformation_template_parameter_code.sh

--- functions/create_cloudformation_template_parameter_code.sh
+++ functions/create_cloudformation_template_parameter_code.sh
@@ -14,17 +14,16 @@ create_cloudformation_template_parameter_code(){
 
     local properties_json=$(jq -r 'if type == "string" then fromjson else . end | .properties' <<< "$SCHEMA") 
     local readOnlyProps=$(jq -r 'if type == "string" then fromjson else . end | if has("readOnlyProperties") then .readOnlyProperties[] else empty end' <<< "$SCHEMA" | sed 's|/properties/||g')
-
-    #echo "Properties json:"
-    #echo $properties_json | jq '.'
     
      while read -r property; do
 
         pcode=""
-        
-        local pjson=$(echo "$properties_json" | jq -r --arg prop "$property" '.[$prop]')
-        echo "Processing property:"
-        echo $pjson | jq .
+
+        if [ $DEBUG ]; then
+          local pjson=$(echo "$properties_json" | jq -r --arg prop "$property" '.[$prop]')
+          echo "Processing parameters property:"
+          echo $pjson | jq .
+        fi
         
         if echo "$readOnlyProps" | grep -q "^$property$"; then
             continue
