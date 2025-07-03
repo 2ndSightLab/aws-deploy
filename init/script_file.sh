bcaa29299e602ea8d1344870e59d37dfa5e25321
Teri Radichel <github@pwn.fyi>
2025-07-03 12:27:31 -0400

    Delete init/script_file.sh

--- init/script_file.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-echo "Generating script file path"
-SCRIPT_FILE_PATH=$(get_script_file_path "$SERVICE_NAME" "$RESOURCE_NAME" "$ACCOUNT" "$REGION" "$GIT_REPO_DIR")
-if [ -z "$SCRIPT_FILE_PATH" ]; then echo "Error: Script file path not set"; exit; fi
-echo "SCRIPT_FILE_PATH: $SCRIPT_FILE_PATH"
-if [ -z "$STACK_FILE_PATH" ]; then "Error: Stack file path not set"; exit; fi
-create_deploy_script_for_resource "$ENV_PROFILE" "$REGION" "$RESOURCE_TYPE" "$SCRIPT_FILE_PATH" "$TEMPLATE_FILE_PATH" "$SCHEMA_B64" "$STACK_NAME" "$STACK_FILE_PATH"
-if [ ! -f $SCRIPT_FILE_PATH ]; then echo "$SCRIPT_FILE_PATH does not exist. Exiting."; exit; fi
