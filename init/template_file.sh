c0ce9a0c5b4df4ea750a53a2241f4fc4210a7b8b
Teri Radichel <github@pwn.fyi>
2025-07-03 12:44:23 -0400

    Create template_file.sh

---
+++ init/template_file.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -e
+echo "Creating template"
+create_cloudformation_template $RESOURCE_NAME $RESOURCE_TYPE $TEMPLATE_FILE_PATH $ENV_PROFILE $REGION $SCHEMA_B64
+if [ ! -f $TEMPLATE_FILE_PATH ]; then echo "$TEMPLATE_FILE_PATH does not exist. Exiting."; exit; fi
