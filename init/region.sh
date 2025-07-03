e3ad7241d5deac81a341a6e0ffae3640cd658a79
Teri Radichel <github@pwn.fyi>
2025-07-02 21:10:13 -0400

    Update region.sh

--- init/region.sh
+++ init/region.sh
@@ -7,7 +7,9 @@ echo "The current region is $REGION. If you want to change the region enter it, 
 read CHANGE_REGION
 trim_spaces_and_quotes $CHANGE_REGION
 
-is_valid_aws_region "$CHANGE_REGION" "$ENV_PROFILE"
+if [ -n "$CHANGE_REGION" ]; then
+  is_valid_aws_region "$CHANGE_REGION" "$ENV_PROFILE"
+fi 
 
 if [ -n "$CHANGE_REGION" ]; then 
   echo "Changing $REGION to $CHANGE_REGION"
