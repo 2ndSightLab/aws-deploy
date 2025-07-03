6b3060ee0235332a151820f5ec737f6d1a79268a
Teri Radichel <github@pwn.fyi>
2025-07-02 22:34:44 -0400

    Update script_file.sh

--- init/script_file.sh
+++ init/script_file.sh
@@ -4,5 +4,6 @@ SCRIPT_FILE_PATH=$(get_script_file_path "$SERVICE_NAME" "$RESOURCE_NAME" "$ACCOU
 if [ -z "$SCRIPT_FILE_PATH" ]; then echo "Error: Script file path not set"; exit; fi
 echo "SCRIPT_FILE_PATH: $SCRIPT_FILE_PATH"
 echo "Creating deploy script for resource: $RESOURCE_TYPE and tempate file: $TEMPLATE_FILE_PATH in region: $REGION"
-create_deploy_script_for_resource "$RESOURCE_TYPE" "$SCRIPT_FILE_PATH" "$TEMPLATE_FILE_PATH" "$ENV_PROFILE" "$REGION" "$SCHEMA_B64"
+create_deploy_script_for_resource "$RESOURCE_TYPE" "$SCRIPT_FILE_PATH" "$TEMPLATE_FILE_PATH" "$ENV_PROFILE" "$REGION" "$SCHEMA_B64" "$STACK_NAME"
+
 if [ ! -f $SCRIPT_FILE_PATH ]; then echo "$SCRIPT_FILE_PATH does not exist. Exiting."; exit; fi
