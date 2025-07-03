8a632406f2650e289740108c43fe28c6eda3696c
Teri Radichel <github@pwn.fyi>
2025-07-03 13:20:59 -0400

    Update create_cloudformation_template_parameter_code.sh

--- functions/create_cloudformation_template_parameter_code.sh
+++ functions/create_cloudformation_template_parameter_code.sh
@@ -84,8 +84,8 @@ create_cloudformation_template_parameter_code(){
             echo "  $property:" >> "$TEMPLATE_FILE_PATH"
             echo "    Type: ${cf_type}" >> "$TEMPLATE_FILE_PATH"
             echo "    Description: Enter value for ${property} ($required)" >> "$TEMPLATE_FILE_PATH"
-            echo $default_value >> "$TEMPLATE_FILE_PATH"
-            echo $allowed_values >> "$TEMPLATE_FILE_PATH"
+            echo "$default_value" >> "$TEMPLATE_FILE_PATH"
+            echo "$allowed_values" >> "$TEMPLATE_FILE_PATH"
           
         fi
         
