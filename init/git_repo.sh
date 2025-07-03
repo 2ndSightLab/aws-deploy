3874bcbe8b25c8a9c8729e25031fb3b0258b6879
Teri Radichel <github@pwn.fyi>
2025-07-03 19:00:04 -0400

    Update git_repo.sh

--- init/git_repo.sh
+++ init/git_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e   
 
-if [ $DEBUG ]; then 
+if [ $DEBUG ]; then
   echo "Configure git repository"
 fi
 
@@ -40,55 +40,60 @@ if [ -z $GIT_REPO_URL ]; then
   
 fi
 
+#if the git repo url is set then clone the repo
+if [ -n "$GIT_REPO_URL" ]; then
+
 GIT_REPO_NAME=$(basename "$GIT_REPO_URL" .git)
-if [ $DEBUG ]; then 
-  echo "GIT_REPO_NAME: $GIT_REPO_NAME"; 
-  if [ -z "$GIT_REPO_NAME" ]; then echo "GIT_REPO_NAME not set"; fi
-fi
+  if [ $DEBUG ]; then 
+    echo "GIT_REPO_NAME: $GIT_REPO_NAME"; 
+    if [ -z "$GIT_REPO_NAME" ]; then echo "GIT_REPO_NAME not set"; fi
+  fi
 
-prompt_git_parent_dir="
-Enter the parent directory where you want to clone $GIT_REPO_URL. 
-Enter for default which clones the repo contents to $HOME/$GIT_REPO_NAME.
-"
-
-GIT_REPO_PARENT_DIR=$(get_env_param_value "$ENV_FILE_PATH" "GIT_REPO_PARENT_DIR")
-if [ -z "$GIT_REPO_PARENT_DIR" ]; then
-  clone="y"
-  read -p "$prompt_git_parent_dir" GIT_REPO_PARENT_DIR
-  if [ -z "$GIT_REPO_PARENT_DIR" ]; then GIT_REPO_PARENT_DIR="$HOME"; fi
-  set_env_param_value "$ENV_FILE_PATH" "GIT_REPO_PARENT_DIR" "$GIT_REPO_PARENT_DIR"
-
+  prompt_git_parent_dir="
+  Enter the parent directory where you want to clone $GIT_REPO_URL. 
+  Enter for default which clones the repo contents to $HOME/$GIT_REPO_NAME.
+  "
+  
   GIT_REPO_PARENT_DIR=$(get_env_param_value "$ENV_FILE_PATH" "GIT_REPO_PARENT_DIR")
-fi
-
-if [ $DEBUG ]; then
-  echo "GIT_REPO_PARENT_DIR: $GIT_REPO_PARENT_DIR"
-fi
-if [ -z "$GIT_REPO_PARENT_DIR" ]; then echo "Error: GIT_REPO_PARENT_DIR is not set in environment file."; ecxit 1; fi
-
-GIT_REPO_DIR="$GIT_REPO_PARENT_DIR/$GIT_REPO_NAME"
-if [ $DEBUG ]; then
-  echo "GIT_REPO_DIR: $GIT_REPO_DIR"
-fi 
-
-pompt_repo_overwrite="
-Repo directory: $GIT_REPO_DIR already exists. Do you want to overwrite it? (y)
-"
-if [ ! -d $GIT_REPO_DIR ]; then #directory doe snot exist
+  if [ -z "$GIT_REPO_PARENT_DIR" ]; then
     clone="y"
-elif [ "$clone" == "y" ];  then #directory exists
-    read -p "$pompt_repo_overwrite " clone
-    if [ "$clone" == "y" ]; then rm -rf $GIT_REPO_DIR; fi
+    read -p "$prompt_git_parent_dir" GIT_REPO_PARENT_DIR
+    if [ -z "$GIT_REPO_PARENT_DIR" ]; then GIT_REPO_PARENT_DIR="$HOME"; fi
+    set_env_param_value "$ENV_FILE_PATH" "GIT_REPO_PARENT_DIR" "$GIT_REPO_PARENT_DIR"
+  
+    GIT_REPO_PARENT_DIR=$(get_env_param_value "$ENV_FILE_PATH" "GIT_REPO_PARENT_DIR")
+  fi
+  
+  if [ $DEBUG ]; then
+    echo "GIT_REPO_PARENT_DIR: $GIT_REPO_PARENT_DIR"
+  fi
+  if [ -z "$GIT_REPO_PARENT_DIR" ]; then echo "Error: GIT_REPO_PARENT_DIR is not set in environment file."; ecxit 1; fi
+  
+  GIT_REPO_DIR="$GIT_REPO_PARENT_DIR/$GIT_REPO_NAME"
+  if [ $DEBUG ]; then
+    echo "GIT_REPO_DIR: $GIT_REPO_DIR"
+  fi 
+  
+  pompt_repo_overwrite="
+  Repo directory: $GIT_REPO_DIR already exists. Do you want to overwrite it? (y)
+  "
+  if [ ! -d $GIT_REPO_DIR ]; then #directory doe snot exist
+      clone="y"
+  elif [ "$clone" == "y" ];  then #directory exists
+      read -p "$pompt_repo_overwrite " clone
+      if [ "$clone" == "y" ]; then rm -rf $GIT_REPO_DIR; fi
+  fi
+  
+  prompt_clone="
+  Clone $GIT_REPO_URL into directory: $GIT_REPO_DIR?
+  "
+  
+  if [ "$clone" == "y" ]; then
+      read -p "$prompt_clone" clone
+      if [ "$clone" = "y" ]; then
+          mkdir -p "$GIT_REPO_PARENT_DIR"
+          git clone "$GIT_REPO_URL" "$GIT_REPO_DIR"
+      fi
+  fi 
+
 fi
-
-prompt_clone="
-Clone $GIT_REPO_URL into directory: $GIT_REPO_DIR?
-"
-
-if [ "$clone" == "y" ]; then
-    read -p "$prompt_clone" clone
-    if [ "$clone" = "y" ]; then
-        mkdir -p "$GIT_REPO_PARENT_DIR"
-        git clone "$GIT_REPO_URL" "$GIT_REPO_DIR"
-    fi
-fi 
