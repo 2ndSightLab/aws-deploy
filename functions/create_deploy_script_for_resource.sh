ae5c6fdac1fb6e49431303e9e9e44ec5fa34e997
Teri Radichel <github@pwn.fyi>
2025-07-02 22:44:09 -0400

    Update create_deploy_script_for_resource.sh

--- functions/create_deploy_script_for_resource.sh
+++ functions/create_deploy_script_for_resource.sh
@@ -48,7 +48,7 @@ create_deploy_script_for_resource() {
     #create the stack file
     #all values can be hardcoded except PARAMETER_OVERRIDES
     #which are set at run time
-    echo "create_stack_file " \
+    echo "create_stack_file \
      \"$RESOURCE_TYPE\" \
      \"$STACK_NAME\" \
      \"$ENV_PROFILE\" \
