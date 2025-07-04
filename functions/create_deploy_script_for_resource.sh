b913e4163b65239f345804186db1f1caac91c1bf
Teri Radichel <github@pwn.fyi>
2025-07-04 02:18:59 -0400

    Update create_deploy_script_for_resource.sh

--- functions/create_deploy_script_for_resource.sh
+++ functions/create_deploy_script_for_resource.sh
@@ -44,6 +44,8 @@ create_deploy_script_for_resource() {
     echo "if [[ -z \"\$PARAMETER_OVERRIDES\" ]]; then" >> "$SCRIPT_FILE_PATH"
     echo "  read -p \"No parameters provided. Continue? (y/n): \" REPLY " >> "$SCRIPT_FILE_PATH"
     echo "  [[ ! \$REPLY =~ ^[Yy]$ ]] && exit 1" >> "$SCRIPT_FILE_PATH"
+
+    local PARAMETER_OVERRIDES_B64=""
     
     echo "else" >> "$SCRIPT_FILE_PATH"
     echo "  # Add base64 encoding of parameter overrides with proper handling of special characters" >> "$SCRIPT_FILE_PATH"
@@ -69,7 +71,7 @@ create_deploy_script_for_resource() {
     
     # Deploy CloudFormation stack
     echo "# Deploy CloudFormation stack" >> "$SCRIPT_FILE_PATH"
-    echo "if [[ -z \"\$PARAMETER_OVERRIDES\" ]]; then" >> "$SCRIPT_FILE_PATH"
+    echo "if [[ -z \"\$PARAMETER_OVERRIDES_B64\" ]]; then" >> "$SCRIPT_FILE_PATH"
     echo "  deploy_cloudformation_stack \$STACK_NAME \$TEMPLATE_FILE_PATH \$ENV_PROFILE \$REGION \"\" \$IAM_CAPABILITY " >> "$SCRIPT_FILE_PATH"
     echo "else" >> "$SCRIPT_FILE_PATH"
     echo "  deploy_cloudformation_stack \$STACK_NAME \$TEMPLATE_FILE_PATH \$ENV_PROFILE \$REGION \$IAM_CAPABILITY \$PARAMETER_OVERRIDES_B64" >> "$SCRIPT_FILE_PATH"
