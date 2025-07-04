e3ab835044296ceac9ea6e6989d8a93bb27393c5
Teri Radichel <github@pwn.fyi>
2025-07-03 21:12:37 -0400

    Update is_valid_aws_profile.sh

--- functions/is_valid_aws_profile.sh
+++ functions/is_valid_aws_profile.sh
@@ -2,20 +2,17 @@
 is_valid_aws_profile() {
 
     local profile="$1"
+    
+    if [ $DEBUG ]; then
+      echo "Checking to see if $profile is a valid AWS CLI profile on this system."
+    fi
+    
+    if [ -n "$AWS_EXECUTION_ENV" ] && [ "$profile" == "default" ];  then
 
-    echo "Checking to see if $profile is a valid AWS CLI profile on this system."
-
-    exists="n"
-    if aws configure list-profiles | grep -q "^${profile}$"; then
-        exists="y"
-    else
-
-        if [ -n "$AWS_EXECUTION_ENV" ] && [ "$profile" == "default" ];  then
-
-          echo "AWS CloudShell environment" >&2
-
-          REGION=$(get_region)
- 
+          if [ $DEBUG ]; then
+            echo "AWS CloudShell environment" >&2
+          fi
+      
           #add default profile
           if [ ! -f $HOME/.aws/ ]; then 
               echo "Create $HOME/.aws/" >&2
@@ -25,14 +22,14 @@ is_valid_aws_profile() {
           echo "region = $REGION" >> ~/.aws/config
           echo "output = json" >> ~/.aws/config
           echo "credential_source = EcsContainer" >> ~/.aws/config
-
-          if aws configure list-profiles | grep -q "^${profile}$"; then
-             echo "Default profile added for CloudShell"
-             exists="y"
-          fi
        fi
     fi
 
+    if aws configure list-profiles | grep -q "^${profile}$"; then
+       echo "Default profile added for CloudShell"
+       exists="y"
+    fi
+    
     if [ "$exists" == "y" ]; then return 0; else return 1; fi
 
 }
